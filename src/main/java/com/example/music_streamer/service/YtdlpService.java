24a3d31ec5e8b5255cc69eafee4bee6f60de2d88
Veeraj12 <109969134+Veeraj12@users.noreply.github.com>
2025-06-17 01:01:42 +0530

    Update YtdlpService.java

--- src/main/java/com/example/music_streamer/service/YtdlpService.java
+++ src/main/java/com/example/music_streamer/service/YtdlpService.java
@@ -15,12 +15,12 @@ public class YtdlpService {
     public AudioResponse extractAudio(String url) {
         try {
             ProcessBuilder builder = new ProcessBuilder(
-                "yt-dlp", "--dump-json", "--no-warnings",
+                "yt-dlp", "--no-warnings",
                 "--cookies", "/app/cookies.txt",
                 "--no-playlist",
                 "--user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
                 "--geo-bypass-country", "IN",
-                url
+              "--print", "url", url
             );
 
             builder.redirectErrorStream(true);
