c63b78989c036df8b08348a486c53e24e02fef44
Veeraj12 <109969134+Veeraj12@users.noreply.github.com>
2025-06-16 23:59:15 +0530

    Update YtdlpService.java

--- src/main/java/com/example/music_streamer/service/YtdlpService.java
+++ src/main/java/com/example/music_streamer/service/YtdlpService.java
@@ -15,7 +15,8 @@ public class YtdlpService {
 		    ProcessBuilder builder = new ProcessBuilder(
 		    "yt-dlp","--dump-json","--no-warnings",
 		    "--cookies", "/app/cookies.txt",
-		    "--no-playlist","--user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) ...",
+		    "--no-playlist","--user-agent", 
+		    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
 		    url
 		);
 	        // ProcessBuilder builder = new ProcessBuilder(
